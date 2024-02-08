f244cfa630b38c85285ce0c08151c812f44880c0
leejihyun-dev <jh_lee@sqisoft.com>
2024-02-08 14:12:48 +0900

    웹소켓 화면 수정 클릭시 해당 위치에 재생시간 보여줌

--- lib/web_soket_test/test_two_screen.dart
+++ lib/web_soket_test/test_two_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:test_screen_change/web_soket_test/web_soket_test.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class TestTwoScreen extends StatefulWidget {
   const TestTwoScreen({super.key});
@@ -9,13 +12,32 @@ class TestTwoScreen extends StatefulWidget {
 }
 
 class _TestTwoScreenState extends State<TestTwoScreen> {
+  late final WebSocketChannel _channel;
+
+  @override
+  void initState() {
+    super.initState();
+    // connectChannel();
+  }
+
+  void connectChannel() {
+    _channel = WebSocketChannel.connect(Uri.parse(
+        'ws://localhost:8080/ws/chat')); // 웹소켓 서버 정보 wss://echo.websocket.events
+    final msg = {
+      "type": "ENTER",
+      "roomId": "1",
+      "name": "이지현???",
+    };
+    _channel.sink.add(jsonEncode(msg));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        color: Colors.amber.shade200,
+        color: Colors.yellow.shade200,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -41,6 +63,41 @@ class _TestTwoScreenState extends State<TestTwoScreen> {
             )
           ],
         ),
+        // child: StreamBuilder(
+        //     stream: _channel.stream,
+        //     builder: (context, snapshot) {
+        //       if (snapshot.hasData) {
+        //         return Column(
+        //           mainAxisAlignment: MainAxisAlignment.center,
+        //           children: [
+        //             Center(
+        //               child: Text(
+        //                 '컨트롤러',
+        //                 style: TextStyle(fontSize: 30),
+        //               ),
+        //             ),
+        //             SizedBox(
+        //               height: 30,
+        //             ),
+        //             ElevatedButton(
+        //               onPressed: () {
+        //                 Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute<void>(
+        //                     builder: (BuildContext context) => WebSoketScreen(),
+        //                   ),
+        //                 );
+        //               },
+        //               child: Text('컨트롤화면 가기'),
+        //             )
+        //           ],
+        //         );
+        //       } else {
+        //         return Center(
+        //           child: Text('연결 실패!'),
+        //         );
+        //       }
+        //     }),
       ),
     );
   }
--- lib/web_soket_test/web_soket_test.dart
+++ lib/web_soket_test/web_soket_test.dart
@@ -24,15 +24,30 @@ class _WebSoketScreenState extends State<WebSoketScreen>
   Duration _totalDuration =
       Duration(hours: 0, minutes: 0, seconds: 0, milliseconds: 0);
 
-  // Duration _duration = Duration.zero;
-  double _duration = 0.0;
+// //테스트
+  double _testduration = 0.0;
+  Duration _duration = Duration.zero;
   Duration _position = Duration.zero;
+  String _minutes = '';
+  String _seconds = '';
+
+  int selectedPosition = 0;
+  bool _selectedTimeVisible = false;
 
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController();
     connectChannel();
+  }
+
+  void selectDelay() {
+    //재생시간 보여줬다가 다시 없애는 애니메이션 주려고 추가함
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _selectedTimeVisible = false;
+      });
+    });
   }
 
   void connectChannel() {
@@ -199,28 +214,7 @@ class _WebSoketScreenState extends State<WebSoketScreen>
                         json.decode(snapshot.data.toString());
                     if (data['sender'] == 'totalLength') {
                       totalLength = data['msg'];
-
-                      // List<String> timeParts = data['playLength'].split(":");
-                      // int hours = int.parse(timeParts[0]);
-                      // int minutes = int.parse(timeParts[1]);
-                      // List<String> secondsAndMilliseconds =
-                      //     timeParts[2].split(".");
-                      // int seconds = int.parse(secondsAndMilliseconds[0]);
-                      // int milliseconds = int.parse(secondsAndMilliseconds[1]);
-                      // print(
-                      //     'dkdhtlqkf $timeParts $hours $minutes $secondsAndMilliseconds $seconds $milliseconds');
-
-                      // _totalDuration = Duration(
-                      //   hours: hours,
-                      //   minutes: minutes,
-                      //   seconds: seconds,
-                      //   milliseconds: milliseconds,
-                      // );
-
-                      // intTotal = int.parse(data['length']);
-                      // print('_totalDuration뭥미 $_totalDuration'); //값이 이상하게 찍혀;
-
-                      _duration =
+                      _testduration =
                           double.parse(data['playLength']); //_totalDuration;
                     }
                     if (data['sender'] == 'currentLength') {
@@ -242,164 +236,141 @@ class _WebSoketScreenState extends State<WebSoketScreen>
                       currentLength = data['msg'];
 
                       _position = _current;
-                      // intCur = int.parse(data['length']);
                     }
-
-                    // int _intTotal =
-                    //     _totalDuration.inSeconds; //클릭한 위치의 재생시간을 알아내기 위해 사용
-                    // print(
-                    //     'snapshot.connectionState 이게 뭐임 ${snapshot.connectionState}');
-                    // if (snapshot.connectionState == ConnectionState.done) {
-                    //   print('재연결 시도');
-                    //   final msg = {
-                    //     "type": "ENTER",
-                    //     "roomId": "1",
-                    //     "name": "이지현???",
-                    //   };
-                    //   _channel.sink.add(jsonEncode(msg));
-                    // }
-
-                    // return Padding(
-                    //   padding: EdgeInsets.only(top: 20, left: 10, right: 10),
-                    //   child: Column(
-                    //     children: [
-                    //       Stack( //LinearProgressIndicator 버전 재생바
-                    //         children: [
-                    //           SizedBox(
-                    //             width: MediaQuery.of(context).size.width,
-                    //             height: 30,
-                    //             child: GestureDetector(
-                    //               onTapDown: (details) {
-                    //                 // 전체 동영상 재생 시간을 초로 가정
-                    //                 double totalDurationInSeconds =
-                    //                     _intTotal.toDouble();
-                    //                 // 선택한 위치를 전체 길이에 대한 백분율로 계산
-                    //                 double selectedPercentage =
-                    //                     details.localPosition.dx /
-                    //                         MediaQuery.of(context).size.width;
-                    //                 // 해당 백분율에 따른 시간을 초로 계산
-                    //                 double selectedTimeInSeconds =
-                    //                     selectedPercentage *
-                    //                         totalDurationInSeconds;
-
-                    //                 // 초를 분과 초로 변환
-                    //                 int selectedMinutes =
-                    //                     (selectedTimeInSeconds / 60).floor();
-                    //                 int selectedSeconds =
-                    //                     (selectedTimeInSeconds % 60).floor();
-
-                    //                 // 분이 60분을 넘어가면 시간을 계산
-                    //                 int selectedHours = 0;
-                    //                 if (selectedMinutes >= 60) {
-                    //                   selectedHours =
-                    //                       (selectedMinutes / 60).floor();
-                    //                   selectedMinutes %= 60;
-                    //                 }
-
-                    //                 print(
-                    //                     '선택한 시간: $selectedHours:$selectedMinutes:$selectedSeconds');
-                    //                 final talkMsg = {
-                    //                   "type": "TALK",
-                    //                   "roomId": "1",
-                    //                   "sender": "videoControl",
-                    //                   "msg":
-                    //                       '$selectedHours:$selectedMinutes:$selectedSeconds',
-                    //                   "length": "",
-                    //                   "playLength": "",
-                    //                 };
-                    //                 _channel.sink.add(jsonEncode(talkMsg));
-                    //               },
-                    //               child: ClipRRect(
-                    //                 borderRadius: const BorderRadius.all(
-                    //                     Radius.circular(20)),
-                    //                 child: LinearProgressIndicator(
-                    //                   backgroundColor: Colors.yellow,
-                    //                   color: Colors.blue,
-                    //                   value: (intCur / intTotal),
-                    //                   minHeight: 30,
-                    //                 ),
-                    //               ),
-                    //             ),
-                    //           ),
-                    //           Align(
-                    //             alignment: Alignment.lerp(
-                    //               Alignment.topLeft,
-                    //               Alignment.topRight,
-                    //               (intCur / intTotal),
-                    //             )!,
-                    //             child: Container(
-                    //               width: 30,
-                    //               height: 30,
-                    //               decoration: BoxDecoration(
-                    //                 borderRadius: BorderRadius.circular(50),
-                    //                 color: Colors.red,
-                    //               ),
-                    //             ),
-                    //           )
-                    //         ],
-                    //       ),
-                    //       Padding(
-                    //         padding: EdgeInsets.only(left: 15),
-                    //         child: Row(
-                    //           children: [
-                    //             Text('$currentLength / '),
-                    //             Text('$totalLength'),
-                    //           ],
-                    //         ),
-                    //       )
-                    //     ],
-                    //   ),
-                    // );
                     return Padding(
                       padding: EdgeInsets.only(top: 20, left: 10, right: 10),
                       child: Column(
                         children: [
-                          SliderTheme(
-                            data: SliderThemeData(
-                              trackHeight: 20, //진행바 높이
-                              activeTrackColor: Colors.blue.shade300, //채워지는 색
-                              inactiveTrackColor: Color(0xFF757575), //진행바
-                              thumbColor: Colors.amber, //진행버튼 색
-                              thumbShape: //진행버튼 크기조절
-                                  RoundSliderThumbShape(enabledThumbRadius: 20),
-                              //
-                              overlayColor: Color(0xFFCDDC39).withOpacity(0.2),
-                              overlayShape: //버튼 눌렀을 때 퍼짐 효과
-                                  RoundSliderOverlayShape(overlayRadius: 30),
-                            ),
-                            child: Slider(
-                              min: 0,
-                              max: _duration,
-                              value: _position.inSeconds.toDouble(),
-                              onChanged: (value) {
-                                // final test = value.toInt();
-                                // // Duration(seconds: value.toInt());
-                                // print('onchanged $value $test');
-                                // // player.seek(position);
+                          Stack(
+                            children: [
+                              Positioned(
+                                //클릭한 위치 재생시간 보여주는거
+                                top: 0,
+                                left: selectedPosition.toDouble(),
+                                child: AnimatedOpacity(
+                                  opacity: _selectedTimeVisible ? 1.0 : 0.0,
+                                  duration: Duration(milliseconds: 300),
+                                  // opacity: _animation,
+                                  // turns: _animationController,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                    child: Padding(
+                                      padding: EdgeInsets.all(5),
+                                      child: Text(
+                                        '$_minutes : $_seconds',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.red,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 30),
+                                child: SliderTheme(
+                                  data: SliderThemeData(
+                                    trackHeight: 20, //진행바 높이
+                                    activeTrackColor:
+                                        Colors.blue.shade300, //채워지는 색
+                                    inactiveTrackColor: Color(0xFF757575), //진행바
+                                    thumbColor: Colors.amber, //진행버튼 색
+                                    thumbShape: //진행버튼 크기조절
+                                        RoundSliderThumbShape(
+                                            enabledThumbRadius: 20),
+                                    //
+                                    overlayColor:
+                                        Color(0xFFCDDC39).withOpacity(0.2),
+                                    overlayShape: //버튼 눌렀을 때 퍼짐 효과
+                                        RoundSliderOverlayShape(
+                                            overlayRadius: 30),
+                                  ),
+                                  child: Slider(
+                                    min: 0,
+                                    max: _testduration,
+                                    value: _position.inSeconds.toDouble(),
+                                    onChanged: (value) {},
+                                    onChangeEnd: (value) {
+                                      setState(() {
+                                        ///이부분은 클릭한 위치에 재생시간 보여주기
+                                        double maxSliderValue = //재생 전체 길이
+                                            _testduration;
+                                        double screenWidth = //현재 화면 너비
+                                            MediaQuery.of(context).size.width;
+                                        double sliderWidth = screenWidth -
+                                            80; // 슬라이더의 좌우 여백을 고려하여 계산
+                                        double left = (value / maxSliderValue) *
+                                            sliderWidth;
 
-                                // final talkMsg = {
-                                //   "type": "TALK",
-                                //   "roomId": "1",
-                                //   "sender": "chee",
-                                //   "msg": 'playProgressbar',
-                                //   "length": "$test",
-                                //   "playLength": "",
-                                // };
-                                // _channel.sink.add(jsonEncode(talkMsg));
-                              },
-                              onChangeEnd: (value) {
-                                final test = value.toInt();
-                                final talkMsg = {
-                                  "type": "TALK",
-                                  "roomId": "1",
-                                  "sender": "chee",
-                                  "msg": 'playProgressbar',
-                                  "length": "$test",
-                                  "playLength": "",
-                                };
-                                _channel.sink.add(jsonEncode(talkMsg));
-                              },
-                            ),
+                                        /** 
+                                       * value / maxSliderValue => Slider의 현재 위치가 전체 재생길이에서 차지하는 비율을 계산한다.
+                                       * (value / maxSliderValue) * sliderWidth => 이 비율에 sliderWidth를 곱해 전체 화면의 위치를 결정한다.
+                                       * slider값이 0일때 화면 좌측 끝, 최대값일때 화면 우측 끝에 위치하며 그 사이 값은 비례하여 중간에 위치하게 된다.
+                                       * 
+                                      */
+
+                                        // Positioned 위젯의 left 값을 업데이트
+                                        selectedPosition = left.toInt();
+
+                                        _selectedTimeVisible = true;
+
+                                        selectDelay();
+
+                                        ////
+                                        //선택한 재생시간 계산하기
+                                        int minutes =
+                                            (value.toInt() / 60).floor();
+                                        int seconds =
+                                            (value.toInt() % 60).floor();
+
+                                        if (minutes < 10) {
+                                          _minutes = '0$minutes';
+                                        } else {
+                                          if (minutes >= 60) {
+                                            //영상 1시간 이상일 경우 처리
+                                            int selectedHours =
+                                                (minutes / 60).floor();
+                                            minutes %= 60;
+                                            String formattedMinutes =
+                                                minutes < 10
+                                                    ? '0$minutes'
+                                                    : '$minutes';
+                                            _minutes =
+                                                '${selectedHours.toString().padLeft(2, '0')} : $formattedMinutes';
+                                          } else {
+                                            //아닐 경우 10분 미만이면 0을 붙여준다.
+                                            _minutes = minutes < 10
+                                                ? '0$minutes'
+                                                : '$minutes';
+                                          }
+                                        }
+                                        if (seconds < 10) {
+                                          _seconds = '0$seconds';
+                                        } else {
+                                          _seconds = '$seconds';
+                                        }
+
+                                        final test = value.toInt();
+                                        final talkMsg = {
+                                          "type": "TALK",
+                                          "roomId": "1",
+                                          "sender": "end",
+                                          "msg": 'playProgressbar',
+                                          "length": "$test",
+                                          "playLength": "",
+                                        };
+                                        _channel.sink.add(jsonEncode(talkMsg));
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                           Padding(
                             padding: EdgeInsets.only(left: 15),
